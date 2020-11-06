de4ac58bad802601536003915a0473cf472f25f2
Plofland <ploflan@gmail.com>
2020-11-06 13:03:54 -0500

    All MVP task code written out

--- index.js
+++ index.js
@@ -209,11 +209,16 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+let newName = "Vincent Van Gogh";
+artists[8].name = newName;
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,8 +230,8 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artists, index) {
+  return `the artist at index ${index} is ${artists[index].name}`
 }  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -236,8 +241,14 @@ it returns an array with names of artists who were born in and died in 20th cent
 example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(artists){
+  let centuryArtist = [];
+  for (let i in artists){
+    if (parseInt(artists[i].years) >= 1900){
+      centuryArtist.push(artists[i].name);
+    }
+  }
+  return centuryArtist;
 }
 
 
@@ -251,10 +262,11 @@ Create a function called `removeArtist` that takes two arguments:
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.  
 */
-function removeArtist(/*Your Code Here*/) {
-   /*Your Code Here*/
+function removeArtist(artists, index) {
+   delete artists[index];
+   return artists.length;
 }
-   
+console.log(removeArtist())
 
 /**
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -271,8 +283,17 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(artists){
+    let newObject = {
+      "id" : 20,
+      "name": "Peter Lofland", 
+      "years": "1989 - 2020",
+      "genre": "Web Design", 
+      "nationality": "American",
+      "bio": "Slap kitten brother with paw need to chase tail. Shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff good morning sunshine so pose purrfectly to show my beauty. Behind the couch whatever and kitty pounce."
+    }
+    artists.push(newObject);
+    return artists;
   }
 
   
@@ -286,8 +307,14 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(artists){
+  let oneHundred = [];
+  for (let i in artists){
+    if (artists[i].paintings > 100){
+      oneHundred.push(artists[i].name);
+    }
+  }
+  return oneHundred;
 }
 
 
